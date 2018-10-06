a2b5fbe1d78bc795787aeb5f80d6794d6348a854
mikeyu4281 <mikeyu4281@gmail.com>
2018-10-06 19:08:59 +0800

    Update DataConsumerController.java

--- src/main/java/com/capgroup/services/DataConsumerController.java
+++ src/main/java/com/capgroup/services/DataConsumerController.java
@@ -14,12 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DataConsumerController {
 
-	@RequestMapping("/sale/store")
-    public void storeSaleTransaction(@RequestParam(name="name", required=false, defaultValue="Java Fan") double name) {
+	@RequestMapping("/test")
+    public void testMethod(@RequestParam(name="name", required=false, defaultValue="Java Fan") double name) {
        
 		System.out.println("Value:" + name);
-		//user?name=Abhijit
-		System.out.println("KK");
+		
 		
     }
 	
